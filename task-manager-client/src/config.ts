86eb484b1bc66c8a241f457fa10a4d4277f664d5
Jose Mendez <jomendezdev@gmail.com>
2020-05-18 18:36:38 -0400

    changed callback port

--- task-manager-client/src/config.ts
+++ task-manager-client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'dev-bqo5cyuc.auth0.com',
   clientId: 'rFObcMHmj2x2azE4ox95vsXQ2EiRF9n9',
-  callbackUrl: 'http://localhost:8101/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
